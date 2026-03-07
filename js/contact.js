3fb86ef7131dfeda53604fd5f3d2a5c25b267d5d
Gayatrimhase021 <gayatrimhase879@gmail.com>
2026-03-07 12:07:32 +0530

    added some info

--- js/contact.js
+++ js/contact.js
@@ -11,3 +11,14 @@ if(inquiries.length===0){
 list.innerHTML="<p>No inquiries yet.</p>";
 return;
 }
+inquiries.forEach(function(data){
+
+let div=document.createElement("div");
+
+div.classList.add("inquiry-item");
+
+div.innerHTML=`
+<b>${data.name}</b> (${data.email})<br>
+${data.message}<br>
+<small>${data.date}</small>
+`;
